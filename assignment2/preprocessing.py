0c2991a9440ef82a23617d992b287f2bf2745736
Afsaneh Towhidi <100664237@oncampus.local>
2018-03-21 22:44:28 -0400

    before going home

--- assignment2/preprocessing.py
+++ assignment2/preprocessing.py
@@ -1,15 +1,15 @@
 import readingData as data
 import pandas as pd
 import re
+import nltk
+#from nltk.stem import PorterStemmer
+#from nltk.corpus import stopwords
+#from nltk.tokenize import wordpunct_tokenize, sent_tokenize,word_tokenize
 
 ### Pre-processing
 data.train = data.train.dropna()
 
-test = data.train[:10]
-
-
-test['title'] = test['title'].str.upper()
-test['Original content'] = test['Original content'].str.upper()
+# data.train = data.train[:10]
 
 
 def change_to_uppercase(x):
@@ -18,51 +18,72 @@ def change_to_uppercase(x):
 def replace_special_char(x):
     return x.replace('??', '\'')
 
-# test['title'].apply(replace_special_char)
-# test['title'].apply(change_to_uppercase)
-print("********************************************* ", test['title'].is_copy)
-
-# print (test['title'])
-# print('----------------')
-
 def removeChar(x):
-    return re.sub('[^A-Za-z0-9\']+', ' ', x)
+    return re.sub('[^A-Za-z\']+', ' ', x)
+#    return re.sub('[^A-Za-z0-9\']+', ' ', x)
 
-test['title'] = test['title'].apply(removeChar)
-print ((test['title']))
+# Change all the letters to uppercase
+data.train['title'] = data.train['title'].str.lower()
+data.train['Original content'] = data.train['Original content'].str.lower()
 
+# Replace undefined character with '
+data.train['title'] = data.train['title'].apply(replace_special_char)
+data.train['Original content'] = data.train['Original content'].apply(replace_special_char)
 
-
-#data.train.to_csv('cleanfile.csv', encoding='utf-8')
-
-# def dftoList(df, name):
-#     lst = []
-#     lst.append(str(df[name][1]))
-#     print(str(df[name][1]))
-#     return lst
-
-
-# dftoList(data.train, 'title')
+# remove punctuations and nonalphabetical characters
+data.train['title'] = data.train['title'].apply(removeChar)
+data.train['Original content'] = data.train['Original content'].apply(removeChar)
 
 
-# from nltk.corpus import stopwords
-# from nltk.tokenize import wordpunct_tokenize
+# Replace words with their lemma
+lemma = nltk.wordnet.WordNetLemmatizer()
 
-# sw = stopwords.words('english')
 
-# def tok_cln(strin):
-#         '''
-#         tokenizes string and removes stopwords
-#         '''
-#         return set(nltk.wordpunct_tokenize(strin)).difference(sw)
+data.train['title'] = data.train['title'].apply(word_tokenize).apply(lambda x : [lemma.lemmatize(y) for y in x])
+data.train['title'] = data.train['title'].apply(lambda x : " ".join(x)) # make a sentence again
 
-# from sklearn.feature_extraction.text import CountVectorizer
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# #docs = ['why hello there', 'omg hello pony', 'she went there? omg']
-# #docs = dftoList(data.train, 'title')
-# vec = CountVectorizer()
-# #vec = TfidfVectorizer()
-# print(countvec.fit_transform(data.train.title))
+data.train['Original content'] = data.train['Original content'].apply(word_tokenize).apply(lambda x : [lemma.lemmatize(y) for y in x])
+data.train['Original content'] = data.train['Original content'].apply(lambda x : " ".join(x)) # make a sentence again
+#print(data.train['title'])
 
-# # df = pd.DataFrame(X.toarray(), columns=vec.get_feature_names())
-# # print(df)
+data.train.to_csv('cleanfile.csv', encoding='utf-8')
+
+
+sw = stopwords.words('english')
+     
+
+from sklearn.feature_extraction.text import CountVectorizer
+data.train['title_without_sw'] = data.train['title'].apply(lambda x: ' '.join([word for word in x.split() if word not in (sw)]))
+data.train['title_without_sw'] = data.train['Original content'].apply(lambda x: ' '.join([word for word in x.split() if word not in (sw)]))
+
+
+countvec = CountVectorizer()
+train_title_matrix = countvec.fit_transform(data.train.title_without_sw)
+
+# train_content_matrix = countvec.fit_transform(data.train.title_without_sw)
+
+#print(countvec.fit_transform(data.train.title))
+#df = pd.DataFrame(data.train['id'], columns)
+#matrix = countvec.fit_transform(data.train.title_without_sw).toarray()
+matrix = pd.DataFrame(countvec.fit_transform(data.train.title_without_sw).toarray(), columns=countvec.get_feature_names())
+#matrix.to_csv('matrix.csv', encoding='utf-8')
+
+
+
+
+#document_term = data.train.copy()
+#print(type(document_term))
+#print(type(matrix))
+#print(pd.concat([document_term, matrix], axis=1))
+#document_term.concat(matrix)
+
+
+
+#df = pd.DataFrame({'title': data.train.title_without_sw['title'],                  
+#                   \countvec.get_feature_names():countvec.fit_transform(data.train.title_without_sw).toarray()})
+
+
+    
+    
+    
+    #document_term.to_csv('termDocument.csv', encoding='utf-8')
--- assignment2/readingData.py
+++ assignment2/readingData.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('2018-ENGR5775-ASSIGNMENT2.csv', encoding = "ISO-8859-1", dtype={'pageviews': float, 'Original content': str,
 'title':str, 'id': float})
-print(len(df))
+#print(len(df))
 
 
 # Split the dataset in 70 to 30 partitions (70% training and 30% test)
